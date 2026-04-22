42292e292f7806d2245039f99e8fc6f7bf36548f
Hug <juicebox.salinas@gamil.com>
2026-04-22 18:15:40 -0400

    nix flake update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
-# flake.nix — import nixpkgs with config and use that pkgs in nixosSystem
 {
+  description = "Hugo's NixOS Config";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }:
@@ -12,25 +15,27 @@
       system = "x86_64-linux";
 
       makePkgs = systemArg: import nixpkgs {
-        inherit systemArg;
+        system = systemArg;
         config = { allowUnfree = true; };
       };
 
       makePkgsUnstable = systemArg: import nixpkgs-unstable {
-        inherit systemArg;
+        system = systemArg;
         config = { allowUnfree = true; };
       };
 
       pkgs = makePkgs system;
     in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nixpkgs-fmt git ];
+      };
+
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [ ./configuration.nix ];
         pkgs = pkgs;
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ nixpkgs-fmt git ];
       };
     };
 }

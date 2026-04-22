7078070cf5e9de8848dad591152c0eb716912fce
Hug <juicebox.salinas@gamil.com>
2026-04-22 18:17:01 -0400

    updated config

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
   programs.firefox.enable = true;
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget

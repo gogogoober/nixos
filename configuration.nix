4d3e7a9e29af6e6d3b2bc0564888f311e55fa0eb
Hug <juicebox.salinas@gamil.com>
2026-04-21 20:46:18 -0400

    flakes are functional

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,7 @@
   environment.systemPackages = with pkgs; [
 	vim
 	git
+  vscode
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];

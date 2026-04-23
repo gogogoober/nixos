15c74848ab85e68198ca1838923df5e60a6ce10f
Hug <juicebox.salinas@gamil.com>
2026-04-22 21:33:52 -0500

    simplified shell

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -28,19 +28,20 @@ in {
       '';
     };
 
-    # Tokyo Night preset — https://starship.rs/presets/tokyo-night
     programs.starship = {
       enable = true;
       settings = {
         "$schema" = "https://starship.rs/config-schema.json";
 
-        format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php$python[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
+        format = "$directory$git_branch$git_status\n$character";
 
         directory = {
-          style = "fg:#e3e5e5 bg:#769ff0";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
+          truncation_length = 4;
           truncation_symbol = "…/";
+          style = "blue";
+          before_repo_root_style = "blue";
+          format = "[$path]($style) ";
+          repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root](blue)[$path](bold white) ";
           substitutions = {
             "Documents" = "󰈙 ";
             "Downloads" = " ";
@@ -50,51 +51,14 @@ in {
         };
 
         git_branch = {
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+          symbol = " ";
+          style = "purple";
+          format = "[$symbol$branch]($style) ";
         };
 
         git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-        };
-
-        nodejs = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        rust = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        golang = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        php = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        python = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        time = {
-          disabled = false;
-          time_format = "%R";
-          style = "bg:#1d2230";
-          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+          style = "red";
+          format = "([$all_status$ahead_behind]($style) )";
         };
       };
     };

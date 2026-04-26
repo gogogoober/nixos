79163185ff19adfaf2ae653754c445d4238fe4c7
Hug <juicebox.salinas@gamil.com>
2026-04-26 15:52:03 -0500

    new default open tab for volume

--- modules/home-manager/hyprland/quick-popups/volume.nix
+++ modules/home-manager/hyprland/quick-popups/volume.nix
@@ -2,7 +2,7 @@
 
 {
   modules.hyprland.popups.volume = {
-    command = "${pkgs.wiremix}/bin/wiremix";
+    command = "${pkgs.wiremix}/bin/wiremix --tab output";
     refreshSignal = 8;
     packages = [ pkgs.wiremix ];
   };
